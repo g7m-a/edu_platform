31e3f1613eac0241eadcb72f2978d44cc63f2b8e
qhys666 <3857236119@qq.com>
2025-10-29 18:53:57 +0800

    Update index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Entry from '../Entry.vue';
-import Login from '../Login.vue';
-import Register from '../Register.vue';
-import HomePage from '../HomePage.vue';
+import StudentLogin from '../StudentLogin.vue';
+import StudentRegister from '../StudentRegister.vue';
+import TeacherLogin from '../TeacherLogin.vue';
+import TeacherRegister from '../TeacherRegister.vue';
+import TeacherHomePage from '../TeacherHomePage.vue';
+import StudentHomePage from '../StudentHomePage.vue';
 
 const routes = [
   { path: '/', redirect: '/Entry' },
   { path: '/Entry', component: Entry },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-  { path: '/HomePage', component: HomePage }
+  { path: '/student-login', component: StudentLogin },
+  { path: '/student-register', component: StudentRegister },
+  { path: '/teacher-login', component: TeacherLogin },
+  { path: '/teacher-register', component: TeacherRegister },
+  { path: '/StudentHomePage', component: StudentHomePage },
+  { path: '/TeacherHomePage', component: TeacherHomePage }
 ];
 
 const router = createRouter({
